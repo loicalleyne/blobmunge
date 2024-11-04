fc7fe09ce61821eb56efbf7ddfc42565eee4bdf6
loicalleyne <loicalleyne@gmail.com>
2024-11-04 13:29:31 -0500

    error codes

--- cmd/main.go
+++ cmd/main.go
@@ -12,49 +12,66 @@ import (
 )
 
 // Reads input data and a base64-encoded bloblang mapping from Stdin
-// (must be separated by '|^^|') and prints the result to Stdout.
+// (must be separated by '}[)(]') and prints the result to Stdout.
 // In the event of errors will output to StdErr and exit with a non-0 code.
 //
 // Example
 // input:
-// {"id":1234,"dev":"12345ert"}|^^|cm9vdC5pZCA9IHRoaXMuaWQKcm9vdC5kZXZpY2UgPSB0aGlzLmRldg==
+// {"id":1234,"dev":"12345ert"}[^^]cm9vdC5pZCA9IHRoaXMuaWQKcm9vdC5kZXZpY2UgPSB0aGlzLmRldg==\n
 // outputs:
 // {"device":"12345ert","id":1234}
+
+const (
+	_                    = iota
+	InputScanError       = iota
+	MissingSeparator     = iota
+	ProblematicInputData = iota
+	EmptySourceData      = iota
+	EmptyMungeMapping    = iota
+	MappingDecodingError = iota
+	MappingParseErro     = iota
+	MungeApplyError      = iota
+)
+
 func main() {
 	l := log.New(os.Stderr, "", log.LstdFlags)
 	reader := bufio.NewReader(os.Stdin)
 	text, err := reader.ReadString('\n')
 	if err != nil {
 		l.Printf("input scan error %v\n", err)
-		os.Exit(1)
+		os.Exit(InputScanError)
 	}
-	lines := strings.Split(text, "|^^|")
+	if !strings.Contains(text, "[^^]") {
+		l.Printf("missing separator\n")
+		os.Exit(MissingSeparator)
+	}
+	lines := strings.Split(text, "[^^]")
 	if len(lines) != 2 {
 		l.Printf("problematic input data\n")
-		os.Exit(2)
+		os.Exit(ProblematicInputData)
 	}
 	if len(lines[0]) == 0 {
 		l.Printf("empty source data\n")
-		os.Exit(3)
+		os.Exit(EmptySourceData)
 	}
 	if len(lines[1]) == 0 {
 		l.Printf("empty munge mapping\n")
-		os.Exit(4)
+		os.Exit(EmptyMungeMapping)
 	}
 	decoded, err := base64.StdEncoding.DecodeString(lines[1])
 	if err != nil {
 		l.Printf("error decoding munge mapping %v\n", err)
-		os.Exit(5)
+		os.Exit(MappingDecodingError)
 	}
 	b, err := blobmunge.New(string(decoded))
 	if err != nil {
 		l.Printf("error parsing munge mapping %v\n", err)
-		os.Exit(6)
+		os.Exit(MappingParseErro)
 	}
 	munged, err := b.ApplyBloblangMapping(lines[0])
 	if err != nil {
 		l.Printf("error applying mapping %v\n", err)
-		os.Exit(7)
+		os.Exit(MungeApplyError)
 	}
 	fmt.Println(string(munged))
 }
